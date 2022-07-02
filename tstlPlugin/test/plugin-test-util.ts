404c843b85d3212e433b6efc9539b300a1aa1d90
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-07-01 17:47:18 -0700

    Move extensions.d.ts into tstlPlugin dir

--- tstlPlugin/test/plugin-test-util.ts
+++ tstlPlugin/test/plugin-test-util.ts
@@ -3,27 +3,37 @@ import * as fs from "fs"
 import { LuaTarget } from "typescript-to-lua"
 import { TapCallback, TestBuilder } from "./tstl-test-util"
 
-const declFiles = ["extensions.d.ts"]
+const srcDir = path.resolve(__dirname, "..")
 
 let declFileContents: Record<string, string> | undefined
-
 function getDeclFileContents(): Record<string, string> {
-  if (declFileContents) return declFileContents
-  declFileContents = {}
-  for (const file of declFiles) {
-    const absolutePath = path.resolve(__dirname, "../../src/declarations", file)
-    declFileContents[file] = fs.readFileSync(absolutePath, "utf8")
+  if (!declFileContents) {
+    declFileContents = {}
+    const files = fs.readdirSync(srcDir).filter((file) => file.endsWith(".d.ts"))
+    for (const file of files) {
+      const filePath = path.join(srcDir, file)
+      declFileContents[filePath] = fs.readFileSync(filePath, "utf8")
+    }
   }
   return declFileContents
 }
 
-const pluginFiles = ["extensions.ts"].map((name) => path.resolve(__dirname, "..", name))
+let pluginFiles: string[] | undefined
+function getPluginFiles(): string[] {
+  if (!pluginFiles) {
+    pluginFiles = fs
+      .readdirSync(srcDir)
+      .filter((file) => file.endsWith("-plugin.ts"))
+      .map((file) => path.join(srcDir, file))
+  }
+  return pluginFiles
+}
 
 export const setupPluginTest: TapCallback = (builder: TestBuilder) => {
   builder.withLanguageExtensions().setOptions({
     strict: true,
     luaTarget: LuaTarget.Lua52,
-    luaPlugins: pluginFiles.map((name) => ({ name })),
+    luaPlugins: getPluginFiles().map((name) => ({ name })),
   })
   for (const [name, content] of Object.entries(getDeclFileContents())) {
     builder.addExtraFile(name, content)
