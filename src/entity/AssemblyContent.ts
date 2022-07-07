354d09f97e254c4ea36a1e667962485e9ec6b4db
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-07-07 15:42:29 -0700

    Add Assembly onChanged event

--- src/entity/AssemblyContent.ts
+++ src/entity/AssemblyContent.ts
@@ -1,13 +1,30 @@
 import { RegisterClass } from "../lib"
 import { Position } from "../lib/geometry"
 import { Map2D, map2dAdd, map2dGet, map2dRemove, MutableMap2D } from "../lib/map2d"
+import { Event } from "../lib/observable"
 import { AssemblyEntity, Entity, MutableAssemblyEntity } from "./AssemblyEntity"
 
 export interface AssemblyContent {
   readonly entities: Map2D<AssemblyEntity>
 
   findCompatible<T extends Entity>(entity: T, position?: Position): AssemblyEntity<T> | nil
+
+  readonly onChanged: Event<AssemblyContentChanged>
 }
+
+export interface AssemblyEntityAdded {
+  readonly type: "add"
+  readonly entity: AssemblyEntity
+}
+
+export interface AssemblyEntityRemoved {
+  readonly type: "remove"
+  readonly entity: AssemblyEntity
+}
+
+// todo: more events
+
+export type AssemblyContentChanged = AssemblyEntityAdded | AssemblyEntityRemoved
 
 export interface MutableAssemblyContent extends AssemblyContent {
   readonly entities: MutableMap2D<MutableAssemblyEntity>
@@ -26,6 +43,7 @@ export function isCompatibleEntity(a: Entity, b: Entity): boolean {
 @RegisterClass("AssemblyContent")
 class AssemblyContentImpl implements MutableAssemblyContent {
   readonly entities: MutableMap2D<MutableAssemblyEntity> = {}
+  readonly onChanged = new Event<AssemblyContentChanged>()
 
   findCompatible(entity: Entity, position?: Position): AssemblyEntity<any> | nil {
     const { x, y } = position ?? entity.position
