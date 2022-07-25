1c4021c052f92d8a1dba565a717d7391bb3fee66
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-07-24 21:10:17 -0700

    Add GPLv3 license

--- src/lib/factoriojsx/jsx.test.tsx
+++ src/lib/factoriojsx/jsx.test.tsx
@@ -1,4 +1,16 @@
+/*
+ * Copyright (c) 2022 GlassBricks
+ * This file is part of BBPP3.
+ *
+ * BBPP3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * BBPP3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { RegisterClass } from "../references"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { FactorioJsx, FCSpec, FlowElementSpec, SpecChildren } from "./index"
 import { Component, Spec } from "./spec"
 
