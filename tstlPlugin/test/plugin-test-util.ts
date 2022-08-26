302f714fe943cbe7e2ab22add36a19085009da53
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-08-25 22:35:29 -0700

    Update copyright, switch to Lesser GPL v3.
    Note: since I own all the code, I may change the license.

--- tstlPlugin/test/plugin-test-util.ts
+++ tstlPlugin/test/plugin-test-util.ts
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2022 GlassBricks
- * This file is part of BBPP3.
+ * This file is part of 100% Blueprint Planning.
  *
- * BBPP3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 100% Blueprint Planning is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
- * BBPP3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 100% Blueprint Planning is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with BBPP3. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License along with 100% Blueprint Planning. If not, see <https://www.gnu.org/licenses/>.
  */
 
 import path = require("path")
