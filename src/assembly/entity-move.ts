8ae58c31ffcef51749ae21e64c416444958140e6
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-09-12 11:59:08 -0700

    Add entity-move

---
+++ src/assembly/entity-move.ts
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2022 GlassBricks
+ * This file is part of 100% Blueprint Planning.
+ *
+ * 100% Blueprint Planning is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * 100% Blueprint Planning is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with 100% Blueprint Planning. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { BBox, Position } from "../lib/geometry"
+
+function canMoveEntity(entity: LuaEntity, position: Position, direction: defines.direction): boolean {
+  return entity.surface.can_place_entity({
+    name: entity.name === "entity-ghost" ? entity.ghost_name : entity.name,
+    position,
+    direction,
+    force: entity.force,
+    build_check_type: defines.build_check_type.blueprint_ghost,
+    inner_name: entity.name === "entity-ghost" ? entity.ghost_name : nil,
+  })
+}
+
+function canWiresReach(this: unknown, entity: LuaEntity) {
+  const wires =
+    entity.type === "electric-pole"
+      ? (entity.neighbours as Record<string, LuaEntity[]>)
+      : entity.circuit_connected_entities
+  if (!wires) return true
+
+  for (const [, entities] of pairs(wires)) {
+    for (const e of entities) {
+      if (!entity.can_wires_reach(e)) return false
+    }
+  }
+  return true
+}
+
+function updateMovedEntity(oldPosition: Position, entity: LuaEntity): void {
+  const proxy = entity.surface.find_entity("item-request-proxy", oldPosition)
+  if (proxy && proxy.valid) proxy.teleport(entity.position)
+
+  entity.update_connections()
+  const updateableEntities = entity.surface.find_entities_filtered({
+    area: BBox.expand(entity.bounding_box, 32),
+    force: entity.force,
+  })
+  for (const e of updateableEntities) e.update_connections()
+}
+
+export type MoveEntityResult = "success" | "overlap" | "could-not-teleport" | "wires-cannot-reach"
+
+/** Moves all entities back if not successful. */
+export function tryMoveAllEntities(
+  entities: LuaEntity[],
+  position: Position,
+  direction: defines.direction,
+): MoveEntityResult {
+  if (!entities.every((e) => canMoveEntity(e, position, direction))) return "overlap"
+  // move
+  const oldPositions = entities.map((e) => e.position)
+  const oldDirections = entities.map((e) => e.direction)
+
+  let teleportError = false
+  for (const entity of entities) {
+    if (entity.teleport(position)) {
+      entity.direction = direction
+    } else {
+      teleportError = true
+      break
+    }
+  }
+  if (teleportError || !entities.every(canWiresReach)) {
+    // revert
+    for (const i of $range(1, entities.length)) {
+      const entity = entities[i - 1]
+      entity.teleport(oldPositions[i - 1])
+      entity.direction = oldDirections[i - 1]
+    }
+    return teleportError ? "could-not-teleport" : "wires-cannot-reach"
+  }
+
+  // update connections
+  for (const i of $range(1, entities.length)) {
+    updateMovedEntity(oldPositions[i - 1], entities[i - 1])
+  }
+
+  return "success"
+}
+
+export function forceMoveEntity(entity: LuaEntity, position: Position, direction: defines.direction): void {
+  const oldPosition = entity.position
+  if (!entity.teleport(position)) {
+    entity.direction = direction
+    return
+  }
+  entity.direction = direction
+  updateMovedEntity(oldPosition, entity)
+  return
+}
---
+++ src/test/assembly/entity-move.test.ts
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2022 GlassBricks
+ * This file is part of 100% Blueprint Planning.
+ *
+ * 100% Blueprint Planning is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * 100% Blueprint Planning is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with 100% Blueprint Planning. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { forceMoveEntity, tryMoveAllEntities } from "../../assembly/entity-move"
+import { createStageSurface, prepareArea } from "../../assembly/surfaces"
+import { BBox } from "../../lib/geometry"
+
+let surfaces: LuaSurface[]
+before_all(() => {
+  surfaces = Array.from({ length: 3 }, () => {
+    const surface = createStageSurface()
+    prepareArea(surface, BBox.around({ x: 0, y: 0 }, 20))
+    return surface
+  })
+})
+
+after_all(() => {
+  surfaces.forEach((s) => game.delete_surface(s))
+})
+
+const origPos = { x: 0.5, y: 0.5 }
+const origDir = defines.direction.east
+let entities: LuaEntity[]
+before_each(() => {
+  entities = surfaces.map((s) =>
+    assert(
+      s.create_entity({
+        name: "inserter",
+        position: origPos,
+        direction: origDir,
+        force: "player",
+      }),
+    ),
+  )
+})
+after_each(() => {
+  entities.forEach((e) => e.destroy())
+})
+
+const newPos = { x: 1.5, y: 1.5 }
+test("can move single entity", () => {
+  const entity = entities[0]
+  forceMoveEntity(entity, newPos, defines.direction.south)
+  assert.same(newPos, entity.position)
+  assert.same(defines.direction.south, entity.direction)
+})
+
+test("can move multiple entities", () => {
+  const result = tryMoveAllEntities(entities, newPos, defines.direction.south)
+  assert.equal("success", result)
+  for (const entity of entities) {
+    assert.same(newPos, entity.position)
+    assert.same(defines.direction.south, entity.direction)
+  }
+})
+
+test("does not move any if any overlaps", () => {
+  const pos = { x: 1.5, y: 1.5 }
+  surfaces[1].create_entity({
+    name: "inserter",
+    position: pos,
+    direction: defines.direction.east,
+  })
+  const result = tryMoveAllEntities(entities, pos, defines.direction.south)
+  assert.equal("overlap", result)
+  for (const entity of entities) {
+    assert.same(origPos, entity.position)
+    assert.same(origDir, entity.direction)
+  }
+})
+
+test("does not move any if wires cannot reach", () => {
+  const pole = surfaces[1].create_entity({
+    name: "small-electric-pole",
+    position: { x: -6, y: -6 }, // barely in range
+  })!
+  assert(
+    pole.connect_neighbour({
+      wire: defines.wire_type.red,
+      target_entity: entities[1],
+    }),
+    "failed to connect",
+  )
+  const result = tryMoveAllEntities(entities, newPos, defines.direction.south)
+  assert.equal("wires-cannot-reach", result)
+  for (const entity of entities) {
+    assert.same(origPos, entity.position)
+    assert.same(origDir, entity.direction)
+  }
+})
