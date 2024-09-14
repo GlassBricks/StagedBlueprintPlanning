d1531d036e7fdb86f3a591f934fd7d85a28db9a2
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-09-13 19:53:06 -0500

    Add clean-broken-entities-staged-bp command

--- src/ui/commands.ts
+++ src/ui/commands.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023 GlassBricks
+ * Copyright (c) 2024 GlassBricks
  * This file is part of Staged Blueprint Planning.
  *
  * Staged Blueprint Planning is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -9,26 +9,22 @@
  * You should have received a copy of the GNU Lesser General Public License along with Staged Blueprint Planning. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { destroy } from "../lib/factoriojsx"
-import { Migrations } from "../lib/migration"
-import "./AllProjects"
-import "./editor-fix"
-import "./opened-entity"
-import "./player-navigation"
-import "./ProjectSettings"
-import "./stage-move-tool"
-import "./toggle-staged-copy"
-import "./edit-blueprint-filters"
-import "./mod-button"
-import "./gps-tag"
-import "./commands"
+import { getAllProjects } from "../project/UserProject"
 
-Migrations.fromAny(() => {
-  for (const [, player] of game.players) {
-    const opened = player.opened
-    if (opened && opened.object_name == "LuaGuiElement" && opened.get_mod() == script.mod_name) {
-      destroy(opened)
-      player.opened = nil
+commands.add_command(
+  "clean-broken-entities",
+  "Staged Blueprint Planning: remove broken entities from all projects. Entities that are broken in their first stage are removed.",
+  () => {
+    let numDeleted = 0
+    for (const project of getAllProjects()) {
+      for (const entity of project.content.allEntities()) {
+        const broken = entity.getWorldEntity(entity.firstStage) == nil
+        if (broken) {
+          project.updates.forceDeleteEntity(entity)
+          numDeleted++
+        }
+      }
     }
-  }
-})
+    game.print(`Deleted ${numDeleted} broken entities.`)
+  },
+)
