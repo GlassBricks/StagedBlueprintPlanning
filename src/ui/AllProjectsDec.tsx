ac60e8820341910d3807e8caa23cee1bfdbddc9f
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2025-11-11 10:25:06 -0600

    chore: Fix prettier formatting

--- src/ui/AllProjectsDec.tsx
+++ src/ui/AllProjectsDec.tsx
@@ -1,8 +1,8 @@
-import { LuaPlayer } from "factorio:runtime";
-import { destroy } from "../lib/factoriojsx";
+import { LuaPlayer } from "factorio:runtime"
+import { destroy } from "../lib/factoriojsx"
 import * as mod_gui from "mod-gui"
 
-export const AllProjectsName = script.mod_name + ":all-projects";
+export const AllProjectsName = script.mod_name + ":all-projects"
 
 export function closeAllProjects(player: LuaPlayer): void {
   destroy(mod_gui.get_frame_flow(player)[AllProjectsName])
