1de01eaf029a5a0b1f07f1af12af0634f3ea4335
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2025-11-24 22:06:43 -0600

    feat: Make new bp paste handling enabled by default

--- src/settings.ts
+++ src/settings.ts
@@ -36,7 +36,7 @@ data.extend<StringSettingDefinition | BoolSettingDefinition>([
     name: Settings.UseBplibForBlueprintPaste,
     type: "bool-setting",
     setting_type: "runtime-per-user",
-    default_value: false,
+    default_value: true,
     order: "f",
   },
   {
